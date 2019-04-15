4a0526ac7eaa30a3ad1793e6bf3fa65c8dce40a7
awalte11 <awalte11@students.towson.edu>
2019-04-15 14:13:38 -0400

    added file and front end specification interfaces
    file interface not complete

---
+++ interface_file.java
@@ -0,0 +1,12 @@
+public interface interface_file {
+	
+	public String getName();
+	
+	public String type(); //This is a placeholder till we figure out how to handle typing
+	
+	public int size();
+	
+	public void rename(String newName);
+	
+	public interface_file(String name, String type);//Placeholder till content sim figured out
+}
